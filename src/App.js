36b995b75d7a388cb608ea2ba0b1dd74883e661e
kishor-karthik-dev <18mech94@gmail.com>
2024-01-21 23:54:56 +0530

    added axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { useState ,useEffect} from "react";
 import "./App.css";
-import { list, list2 } from "./assets/cards-list";
+// import { list, list2 } from "./assets/cards-list";
 import Cards from "./components/Cards";
 import Filter from "./components/Filter";
 import Header from "./components/Header";
-
+import Axios from "axios";
 function App() {
   const [selectedFilter, setSelectedFilter] = useState(0);
+  const [list, setList] = useState([]);
+  useEffect(()=>{
+    Axios.get('https://simple-backend-ekc3.onrender.com/gethotels')
+    .then((res)=>{
+      console.log(res.data);
+      setList(res.data);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  })
   return (
     <div className="App">
       <Header />
@@ -14,7 +25,8 @@ function App() {
         selectedFilter={selectedFilter}
         setSelectedFilter={setSelectedFilter}
       />
-      {selectedFilter == 0 ? <Cards list={list} /> : <Cards list={list2} />}
+      {selectedFilter == 0 ? <Cards list={list} /> : <Cards list={list} />}
+      {/* <Cards list={list} /> */}
     </div>
   );
 }
